2a22626178c3221912a315ed91ef3b13d0f470f2
Daisy Llivisaca <daisynohelia@gmail.com>
2025-12-16 17:48:38 -0500

    push 41: se configuraron titulo dinamicos para la pestaña del navegador por cada paso de agendamiento

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -474,6 +474,14 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
+        const stepTitles = {
+            1: "Área de atención · FamySalud",
+            2: "Servicio · FamySalud",
+            3: "Profesional · FamySalud",
+            4: "Fecha y hora · FamySalud",
+            5: "Datos del paciente · FamySalud",
+            6: "Pago · FamySalud"
+        };
         $(document).ready(function() {
 
             const categories = @json($categories);
@@ -912,6 +920,10 @@
                 $(".booking-container")[0].scrollIntoView({
                     behavior: "smooth"
                 });
+
+                 if (stepTitles[step]) {
+                    document.title = stepTitles[step];
+                }
             }
 
 
