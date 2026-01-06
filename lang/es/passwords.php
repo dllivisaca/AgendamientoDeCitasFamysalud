6f90b7f6b67ae9161ed69028cb0343ee547d2bf0
Daisy Llivisaca <daisynohelia@gmail.com>
2026-01-06 10:31:37 -0500

    push 151: en el push anterior tbn cambiamos el idioma a es, y en este push agregamos textos para cuando se envia link de recuperacion de contraseña y similares, ya esta completa la pagina de recuperacion de contraseña, toca continuar ocultando el link de registro desde la app inicial y bloquear acceso por url

---
+++ lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Your password has been reset.',
+    'sent' => 'Te hemos enviado un enlace para restablecer tu contraseña.',
+    'throttled' => 'Por favor espera antes de intentarlo de nuevo.',
+    'token' => 'Este token de restablecimiento no es válido.',
+    'user' => 'No existe un usuario con ese correo electrónico.',
+];
